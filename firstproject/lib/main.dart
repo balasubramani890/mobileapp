a4f3e3232865ecf482d73196802b47ca9a208df1
balask <balask@gmail.com>
2022-09-04 22:36:46 +0530

    Sample App Bar with title body icons

--- firstproject/lib/main.dart
+++ firstproject/lib/main.dart
@@ -2,24 +2,54 @@ import 'package:flutter/material.dart';
 
 void main()
 {
-  runApp(MyFirstAppDemo());
+  runApp(SampleAppBar());
 }
 
-class MyFirstAppDemo extends StatefulWidget {
-  const MyFirstAppDemo({Key? key}) : super(key: key);
+class SampleAppBar extends StatefulWidget {
+  const SampleAppBar({Key? key}) : super(key: key);
 
   @override
-  State<MyFirstAppDemo> createState() => _MyFirstAppDemoState();
+  State<SampleAppBar> createState() => _SampleAppBarState();
 }
 
-class _MyFirstAppDemoState extends State<MyFirstAppDemo> {
+class _SampleAppBarState extends State<SampleAppBar> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Text('Senthil Balu'),
-      ),
-    );
+      title: "Senthil",
+      home: Scaffold(
+        appBar: AppBar(
+
+          leading: IconButton(
+            icon: Icon(Icons.menu), onPressed: () {
+          },
+          ),
+          actions: <Widget>[
+            IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.more_vert))
+          ],
+          flexibleSpace: SafeArea(
+            child: Icon(
+              Icons.ac_unit
+            ),
+          ),
+            bottom: PreferredSize(
+            child: Container(
+            color: Colors.red,
+            height: 50,
+              width: double.infinity,
+              child: Text("Senthil Balu"),
+        ),
+        preferredSize: Size.fromHeight(50),
+        ),
+          ),
+        body: Center(child: Text("Senthil Balu", style: TextStyle(fontSize: 25),)),
+        floatingActionButton: FloatingActionButton(onPressed: () {  },
+          elevation: 25.0,
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        ),
+      );
   }
 }
-
